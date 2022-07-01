89c6499b0890243d2f6821bf42b29802e7f64b38
Nandkishor146 <108401658+Nandkishor146@users.noreply.github.com>
2022-07-01 23:30:20 +0530

    Main
    MainFile

---
+++ SpringBootproductApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootproductApplication.class, args);
+	}
+
+}
