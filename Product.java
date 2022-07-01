589a4c1b722f792fcbad700a36cfa2d480403904
Nandkishor146 <108401658+Nandkishor146@users.noreply.github.com>
2022-07-01 23:27:19 +0530

    Entity
    ProductBean

---
+++ Product.java
@@ -0,0 +1,59 @@
+package ProductBean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column( name ="id")
+	private int id;
+	
+	@Column( name ="id")
+	private String name;
+	
+	@Column( name ="id")
+	private double price;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Product(int id, String name, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public Product() {
+
+	}
+
+}
